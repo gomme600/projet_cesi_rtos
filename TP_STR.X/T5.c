aeecd6a7ecffb69903487d8bce7b3fdf383484e6
gomme600 <gomme600@hotmail.com>
2022-10-05 16:00:33 +0200

    update

--- TP_STR.X/T5.c
+++ TP_STR.X/T5.c
@@ -1,5 +1,23 @@
 #include "T5.h"
 
+// Function to set the kth bit of n
+char setBit(char n, int k)
+{
+    return (n | (1 << k));
+}
+  
+// Function to clear the kth bit of n
+char clearBit(char n, int k)
+{
+    return (n & (~(1 << k)));
+}
+  
+// Function to toggle the kth bit of n
+char toggleBit(char n, int k)
+{
+    return (n ^ (1 << k));
+}
+
 //Alarme
 
 int buz=1;
@@ -12,24 +30,53 @@ void tache5()
         
         //Si personne dans siege et vitesse > 0
         if (SIEGE==1 && vitesse>0)
-            alarme_active |= (1 << 6);
+            alarme_active = setBit(alarme_active, 6);
+            //alarme_active |= (1 << 6);
         else
-            alarme_active &= (0 << 6);
+            alarme_active = clearBit(alarme_active, 6);
+            //alarme_active &= (0 << 6);
         
         if (n_octet_badge==0)
-            alarme_active |= (1 << 4);
+            alarme_active = setBit(alarme_active, 4);
+            //alarme_active |= (1 << 4);
         else
-            alarme_active &= (0 << 4);
+            alarme_active = clearBit(alarme_active, 4);
+            //alarme_active &= (0 << 4);
         
         if (CHOC==0)
-            alarme_active |= (1 << 2);
+            alarme_active = setBit(alarme_active, 2);
+            //alarme_active |= (1 << 2);
         else
-            alarme_active &= (0 << 2);
+            alarme_active = clearBit(alarme_active, 2);
+            //alarme_active &= (0 << 2);
         
-        if (FREIN_A_MAIN==0)
-            alarme_active |= (1 << 5);
+        if (FREIN_A_MAIN==0 && vitesse>0)
+            alarme_active = setBit(alarme_active, 5);
+            //alarme_active |= (1 << 5);
         else
-            alarme_active &= (0 << 5);
+            alarme_active = clearBit(alarme_active, 5);
+            //alarme_active &= (0 << 5);
+        
+        if (lecture_8bit_analogique(TEMPERATURE_EAU)>200)
+            alarme_active = setBit(alarme_active, 0);
+            //alarme_active |= (1 << 0);
+        else
+            alarme_active = clearBit(alarme_active, 0);
+            //alarme_active &= (0 << 0);
+        
+        if (lecture_8bit_analogique(TEMPERATURE_HUILE)>200)
+            alarme_active = setBit(alarme_active, 1);
+            //alarme_active |= (1 << 1);
+        else
+            alarme_active = clearBit(alarme_active, 1);
+            //alarme_active &= (0 << 1);
+        
+        if (batterie<25)
+            alarme_active = setBit(alarme_active, 3);
+            //alarme_active |= (1 << 3);
+        else
+            alarme_active = clearBit(alarme_active, 3);
+            //alarme_active &= (0 << 3);
         
         // Buzzer alume
         buz=!buz;
